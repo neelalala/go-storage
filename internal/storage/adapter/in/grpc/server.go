bce8e563c140a7940c49e44035b48cc92d815d43
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-07-06 10:26:58 +0700

    storage grpc server

---
+++ internal/storage/adapter/in/grpc/server.go
@@ -0,0 +1,131 @@
+package grpc
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net"
+
+	"github.com/neelalala/go-storage/internal/storage/domain"
+	storagepb "github.com/neelalala/go-storage/pkg/proto/storage"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type Storage interface {
+	SaveObject(ctx context.Context, obj domain.Object) error
+	GetObject(ctx context.Context, name string) (domain.Object, error)
+	DeleteObject(ctx context.Context, name string) error
+}
+
+type Server struct {
+	storagepb.UnimplementedStorageServer
+	addr       string
+	grpcServer *grpc.Server
+	storage    Storage
+
+	log *slog.Logger
+}
+
+func NewServer(addr string, storage Storage, log *slog.Logger) *Server {
+	grpcServer := grpc.NewServer()
+
+	server := &Server{
+		addr:       addr,
+		grpcServer: grpcServer,
+		storage:    storage,
+		log:        log,
+	}
+
+	storagepb.RegisterStorageServer(grpcServer, server)
+
+	return server
+}
+
+func (s *Server) Start() error {
+	listener, err := net.Listen("tcp", s.addr)
+	if err != nil {
+		return fmt.Errorf("failed to listen on address %s: %w", s.addr, err)
+	}
+
+	s.log.Info("gRPC server is running", "address", s.addr)
+	if err := s.grpcServer.Serve(listener); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Server) Stop(ctx context.Context) error {
+	s.log.Info("Shutting down gRPC server...")
+
+	stopped := make(chan struct{})
+	go func() {
+		s.grpcServer.GracefulStop()
+		close(stopped)
+	}()
+
+	select {
+	case <-ctx.Done():
+		s.grpcServer.Stop()
+		return ctx.Err()
+	case <-stopped:
+		return nil
+	}
+}
+
+func (s *Server) SaveObject(ctx context.Context, req *storagepb.SaveRequest) (*emptypb.Empty, error) {
+	s.log.Debug("save object request")
+
+	obj := domain.Object{
+		Name: req.GetObject().GetName(),
+		Data: req.GetObject().GetData(),
+	}
+
+	err := s.storage.SaveObject(ctx, obj)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "error saving object: %v", err)
+	}
+
+	return nil, nil
+}
+
+func (s *Server) GetObject(ctx context.Context, req *storagepb.GetRequest) (*storagepb.GetResponse, error) {
+	s.log.Debug("get object request")
+
+	obj, err := s.storage.GetObject(ctx, req.GetName())
+	if err != nil {
+		if errors.Is(err, domain.ErrFileNotFound) {
+			return nil, status.Errorf(codes.InvalidArgument, "%v", err)
+		}
+
+		return nil, status.Errorf(codes.Internal, "error getting object: %v", err)
+	}
+
+	resp := &storagepb.GetResponse{
+		Object: &storagepb.Object{
+			Name: obj.Name,
+			Data: obj.Data,
+		},
+	}
+
+	return resp, nil
+}
+
+func (s *Server) DeleteObject(ctx context.Context, req *storagepb.DeleteRequest) (*emptypb.Empty, error) {
+	s.log.Debug("delete object request")
+
+	err := s.storage.DeleteObject(ctx, req.GetName())
+	if err != nil {
+		if errors.Is(err, domain.ErrFileNotFound) {
+			return nil, status.Errorf(codes.InvalidArgument, "%v", err)
+		}
+
+		return nil, status.Errorf(codes.Internal, "error deleting object: %v", err)
+	}
+
+	return nil, nil
+}
