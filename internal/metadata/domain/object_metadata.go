19142eacfa5a67372943d023d0fce690f5bec6ed
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-07-09 00:20:38 +0700

    use uuid.UUID instead of string

--- internal/metadata/domain/object_metadata.go
+++ internal/metadata/domain/object_metadata.go
@@ -1,13 +1,43 @@
 package domain
 
-import "time"
+import (
+	"time"
 
-type ObjectMetadata struct {
+	"github.com/google/uuid"
+)
+
+type Object struct {
 	Bucket        string
 	Key           string
+	ObjectPath    string
 	Size          uint64
 	Checksum      uint32
+	StorageNodeID uuid.UUID
 	CreatedAt     time.Time
 	UpdatedAt     time.Time
-	StorageNodeID string
+}
+
+type Upload struct {
+	UploadID      uuid.UUID
+	Bucket        string
+	Key           string
+	ObjectPath    string
+	StorageNodeID uuid.UUID
+	CreatedAt     time.Time
+}
+
+type Status string
+
+const (
+	StatusPending Status = "PENDING"
+	StatusError   Status = "ERROR"
+)
+
+type GCTask struct {
+	DeletionID    int64
+	ObjectPath    string
+	StorageNodeID uuid.UUID
+	Status        Status
+	Attempts      int
+	CreatedAt     time.Time
 }
