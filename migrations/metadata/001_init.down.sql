2aba84990c68a8df2cf6b1f3c125feedb2629566
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-07-09 00:19:59 +0700

    migrations
    обдумал архитектуру приложения и пришлось сильно менять схему бд

--- migrations/metadata/001_init.down.sql
+++ migrations/metadata/001_init.down.sql
@@ -1,5 +1,9 @@
-DROP TRIGGER IF EXISTS update_obkect_metadata_modtime ON object_metadata;
+DROP TABLE IF EXISTS uploads;
+
+DROP TABLE IF EXISTS gc_queue;
+
+DROP TRIGGER IF EXISTS update_object_modtime ON objects;
 
 DROP FUNCTION IF EXISTS update_modified_column();
 
-DROP TABLE IF EXISTS object_metadata;
+DROP TABLE IF EXISTS objects;
--- migrations/metadata/001_init.up.sql
+++ migrations/metadata/001_init.up.sql
@@ -1,11 +1,12 @@
-CREATE TABLE IF NOT EXISTS object_metadata (
+CREATE TABLE IF NOT EXISTS objects (
   bucket TEXT NOT NULL,
   key TEXT NOT NULL,
+  object_path TEXT NOT NULL,
   size BIGINT NOT NULL,
   checksum BIGINT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  storage_node_id TEXT NOT NULL, 
+  storage_node_id UUID NOT NULL, 
 
   PRIMARY KEY (bucket, key),
 
@@ -15,10 +16,10 @@ CREATE TABLE IF NOT EXISTS object_metadata (
 
   CONSTRAINT valid_key CHECK (
     key <> ''
-  )
+  ),
 
-  CONSTRAINT valid_storage_node_id CHECK (
-    storage_node_id <> ''
+  CONSTRAINT valid_object_path CHECK (
+    object_path <> ''
   )
 );
 
@@ -34,3 +35,42 @@ CREATE TRIGGER update_object_metadata_modtime
   BEFORE UPDATE ON object_metadata
   FOR EACH ROW
   EXECUTE PROCEDURE update_modified_column();
+
+CREATE TABLE IF NOT EXISTS uploads (
+  upload_id UUID PRIMARY KEY,
+  bucket TEXT NOT NULL,
+  key TEXT NOT NULL,
+  object_path TEXT NOT NULL,
+  storage_node_id UUID NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  CONSTRAINT valid_bucket CHECK (
+    bucket <> ''  
+  ),
+
+  CONSTRAINT valid_key CHECK (
+    key <> ''
+  ),
+
+  CONSTRAINT valid_object_path CHECK (
+    object_path <> ''
+  )
+);
+
+
+CREATE TABLE IF NOT EXISTS gc_queue (
+  deletion_id BIGSERIAL PRIMARY KEY,
+  object_path TEXT NOT NULL,
+  storage_node_id UUID NOT NULL,
+  status TEXT NOT NULL DEFAULT 'PENDING',
+  attempts INT NOT NULL DEFAULT 0,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  CONSTRAINT valid_object_path CHECK (
+    object_path <> ''
+  ),
+
+  CONSTRAINT valid_status CHECK (
+    status IN ('PENDING', 'ERROR')
+  )
+);
