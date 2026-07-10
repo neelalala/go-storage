1c13b309dbbdae9ac66eb3c4a11368b80926b7a4
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-07-10 22:37:51 +0700

    CreateUpload gets Upload struct as param
    since this method actually needs not only bucket and key but also
    object_path and storage_node_id, i think passing this struct is ok but
    still not modifing it

--- internal/metadata/domain/repository.go
+++ internal/metadata/domain/repository.go
@@ -11,7 +11,7 @@ type Transactor interface {
 }
 
 type UploadRepository interface {
-	CreateUpload(ctx context.Context, bucket, key string) (*Upload, error)
+	CreateUpload(ctx context.Context, upload Upload) (*Upload, error)
 	DeleteUpload(ctx context.Context, uploadID uuid.UUID) error
 	CommitUpload(ctx context.Context, uploadID uuid.UUID, size uint64, checksum uint32) error
 }
