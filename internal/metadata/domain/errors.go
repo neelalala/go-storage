1a5480a969efc2601a77fee75dae9d3a6084e820
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-07-11 10:10:04 +0700

    err upload not found

--- internal/metadata/domain/errors.go
+++ internal/metadata/domain/errors.go
@@ -2,4 +2,7 @@ package domain
 
 import "errors"
 
-var ErrObjectNotFound = errors.New("error: object not found")
+var (
+	ErrObjectNotFound = errors.New("error: object not found")
+	ErrUploadNotFound = errors.New("error: upload not found")
+)
