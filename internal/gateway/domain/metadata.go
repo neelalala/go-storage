d2d4d9284b51f98addce964f8edd12994aa73e32
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-07-15 14:43:45 +0700

    metadata interface

---
+++ internal/gateway/domain/metadata.go
@@ -0,0 +1,38 @@
+package domain
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type MetadataService interface {
+	InitUpload(ctx context.Context, bucket, key string, size uint64) (Upload, StorageNode, error)
+	CommitUpload(ctx context.Context, uploadID uuid.UUID, checksum uint32) error
+	AbortUpload(ctx context.Context, uploadID uuid.UUID) error
+	GetObject(ctx context.Context, bucket, key string) (ObjectMetadata, StorageNode, error)
+	GetObjects(ctx context.Context, bucket, path string, limit, offset int) ([]ObjectMetadata, error)
+	DeleteObject(ctx context.Context, bucket, key string) error
+}
+
+type ObjectMetadata struct {
+	Bucket        string
+	Key           string
+	ObjectPath    string
+	Size          uint64
+	Checksum      uint32
+	StorageNodeID uuid.UUID
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
+
+type Upload struct {
+	UploadID      uuid.UUID
+	Bucket        string
+	Key           string
+	ObjectPath    string
+	Size          uint64
+	StorageNodeID uuid.UUID
+	CreatedAt     time.Time
+}
