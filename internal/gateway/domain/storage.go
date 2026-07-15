ec71e0220a87f8f4688c87ea8548188ac83db773
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-07-15 14:42:20 +0700

    storage node struct and node manager interface

--- internal/gateway/domain/storage.go
+++ internal/gateway/domain/storage.go
@@ -1,9 +1,23 @@
 package domain
 
-import "context"
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
 
 type Storage interface {
 	SaveObject(ctx context.Context, object Object) (uint32, error)
 	GetObject(ctx context.Context, name string) (Object, error)
 	DeleteObject(ctx context.Context, name string) error
+}
+
+type StorageNode struct {
+	ID      uuid.UUID
+	Address string
+}
+
+type StorageNodeManager interface {
+	GetStorage(address string) (Storage, error)
+	Invalidate(address string)
 }
