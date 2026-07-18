ed34968b0446f7fcc221ef74dad5e3175e7f6465
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-07-18 13:34:35 +0700

    add errors for bucket repo

--- internal/metadata/domain/errors.go
+++ internal/metadata/domain/errors.go
@@ -3,6 +3,9 @@ package domain
 import "errors"
 
 var (
-	ErrObjectNotFound = errors.New("error: object not found")
-	ErrUploadNotFound = errors.New("error: upload not found")
+	ErrBucketExists    = errors.New("error: bucket already exists")
+	ErrBucketNotExists = errors.New("error: bucket not exists")
+	ErrBucketNotEmpty  = errors.New("error: bucket bot empty")
+	ErrObjectNotFound  = errors.New("error: object not found")
+	ErrUploadNotFound  = errors.New("error: upload not found")
 )
