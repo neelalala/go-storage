e9c90d850c3e658295bc387434dec042c8715bf4
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-07-23 12:23:25 +0700

    repos documentation

--- internal/metadata/domain/repository.go
+++ internal/metadata/domain/repository.go
@@ -11,23 +11,33 @@ type Transactor interface {
 }
 
 type BucketRepository interface {
+	// GetBuckets may return next domain errors: ErrAccessDenied
 	GetBuckets(ctx context.Context, userID uuid.UUID, limit, offset int) ([]Bucket, error)
+	// CreateBucket may return next domain errors: ErrBucketAlreadyExists
 	CreateBucket(ctx context.Context, userID uuid.UUID, name string) (Bucket, error)
+	// DeleteBucket may return next domain errors: ErrBucketNotEmpty
 	DeleteBucket(ctx context.Context, name string) error
+	// GetBucket may return next domain errors: ErrBucketNotExists
 	GetBucket(ctx context.Context, name string) (Bucket, error)
 }
 
 type UploadRepository interface {
+	// GetUpload may return next domain errors: ErrUploadNotExists
 	GetUpload(ctx context.Context, uploadID uuid.UUID) (Upload, error)
+	// CreateUpload may return next domain errors: ErrBucketNotExists
 	CreateUpload(ctx context.Context, upload Upload) (Upload, error)
+	// DeleteUpload may return next domain errors: ErrUploadNotExists
 	DeleteUpload(ctx context.Context, uploadID uuid.UUID) error
 	// CommitUpload may return next domain errors: ErrUploadNotExists
 	CommitUpload(ctx context.Context, uploadID uuid.UUID, hash string) error
 }
 
 type ObjectRepository interface {
+	// GetObject may return next domain errors: ErrAccessDenied, ErrBucketNotExists, ErrObjectNotFound
 	GetObject(ctx context.Context, userID uuid.UUID, bucket, key string) (Object, error)
+	// SoftDeleteObject may return next domain errors: ErrBucketNotExists, ErrBucketNotEmpty
 	SoftDeleteObject(ctx context.Context, bucket, key string) error
+	// GetObjects may return next domain errors: ErrBucketNotExists
 	GetObjects(ctx context.Context, bucket, path, delimiter string, limit, offset int) ([]Object, error)
 }
 
