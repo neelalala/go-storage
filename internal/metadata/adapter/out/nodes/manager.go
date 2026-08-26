48d663efa85a0d25c2a6c1e13518b51be33910c2
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-08-26 22:41:45 +0700

    node manager and registry
    refactor code: replace RoundRobinStaticRegistry with two different
    components: registry (contains all alive nodes) and manager (gives next
    node to use)

---
+++ internal/metadata/adapter/out/nodes/manager.go
@@ -0,0 +1,47 @@
+package nodes
+
+import (
+	"context"
+	"slices"
+	"strings"
+	"sync/atomic"
+
+	"github.com/neelalala/go-storage/internal/metadata/domain"
+)
+
+type NodeRegistry interface {
+	GetAllNodes(context.Context) ([]domain.StorageNode, error)
+}
+
+type RoundRobinNodeManager struct {
+	registry NodeRegistry
+
+	counter atomic.Uint64
+}
+
+func NewRoundRobinNodeManager(registry NodeRegistry) *RoundRobinNodeManager {
+	return &RoundRobinNodeManager{
+		registry: registry,
+	}
+}
+
+func (m *RoundRobinNodeManager) NextNode(ctx context.Context) (domain.StorageNode, error) {
+	nodes, err := m.registry.GetAllNodes(ctx)
+	if err != nil {
+		return domain.StorageNode{}, err
+	}
+
+	if len(nodes) == 0 {
+		return domain.StorageNode{}, domain.ErrNoStorageNodes
+	}
+
+	slices.SortFunc(nodes, func(a, b domain.StorageNode) int {
+		return strings.Compare(a.ID.String(), b.ID.String())
+	})
+
+	idx := m.counter.Add(1)
+
+	node := nodes[idx%uint64(len(nodes))]
+
+	return node, nil
+}
