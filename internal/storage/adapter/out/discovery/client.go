a6b0caa0cf65b1611b003703bc7019d8e2c92280
Semen Dutkin <s.dutkin@g.nsu.ru>
2026-08-27 00:02:20 +0700

    discovery service client

---
+++ internal/storage/adapter/out/discovery/client.go
@@ -0,0 +1,44 @@
+package discovery
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	metadatapb "github.com/neelalala/go-storage/pkg/proto/metadata"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Client struct {
+	client metadatapb.NodeDiscoveryClient
+	conn   *grpc.ClientConn
+}
+
+func New(addr string) (*Client, error) {
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		client: metadatapb.NewNodeDiscoveryClient(conn),
+		conn:   conn,
+	}, nil
+}
+
+func (c *Client) Close() error {
+	return c.conn.Close()
+}
+
+func (c *Client) Heartbeat(ctx context.Context, id uuid.UUID) error {
+	req := &metadatapb.HeartbeatRequest{
+		NodeId: id.String(),
+	}
+
+	_, err := c.client.Heartbeat(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
